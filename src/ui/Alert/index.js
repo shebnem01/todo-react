e79f8be534191a5d1f15d913ab6c722d0e1edee4
shebnem <rshebnem01@gmail.com>
2023-07-12 06:10:16 +0400

    todo app created

--- src/ui/Alert/index.js
+++ src/ui/Alert/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import styles from "./Alert.module.css";
+import PropTypes from "prop-types";
 class Alert extends Component {
   render() {
+    let { variant, children } = this.props;
     return (
-      <div className={`${styles.alert} ${styles[this.props.variant]}`}>
-        {this.props.children}
-      </div>
+      <div className={`${styles.alert} ${styles[variant]}`}>{children}</div>
     );
   }
 }
 export default Alert;
+Alert.propTypes = {
+  variant: PropTypes.string,
+  children: PropTypes.node,
+};
